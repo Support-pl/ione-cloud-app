c50091476b67d63d7c68dee343595d4d6a735068
dowellkin <dowellkin@gmail.com>
2020-09-10 23:20:56 +0300

    new VDC update: added new option fields

--- src/components/appMain/newVDC.vue
+++ src/components/appMain/newVDC.vue
@@ -12,7 +12,7 @@
 							Выберите ОС:
 						</a-divider>
 						<div class="newCloud__template">
-							<div class="newCloud__template-item" :class="{ active: activeOS==OS.id }" @click='()=> activeOS = OS.id' v-for="OS in templatesArray" :key="OS.id">
+							<div v-for="OS in templatesArray" class="newCloud__template-item" :class="{ active: activeOS==OS.id }" @click='()=> activeOS = OS.id' :key="OS.id">
 								<div class="newCloud__template-image">
 									<img :src="'https://vcloud.support.by/' + OS.logo" :alt="OS.name">
 								</div>
@@ -20,40 +20,146 @@
 							</div>
 						</div>
 					</div>
+
 					<div class="newCloud__option-field">
+
 						<a-divider orientation="left">
 							CPU + RAM:
 						</a-divider>
-						<div style="margin-bottom: 16px">
-							RAM: <a-input-number default-value="1">
-								<a-select slot="addonAfter" default-value="GB" style="width: 80px">
-									<a-select-option value="GB">
-										GB
-									</a-select-option>
-									<a-select-option value="MB">
-										MB
-									</a-select-option>
-								</a-select>
-							</a-input-number>
-						</div>
-						<div style="margin-bottom: 16px">
-							CPU: <a-input-number default-value="1">
-								<a-select slot="addonAfter" default-value="GB" style="width: 80px">
-									<a-select-option value="GB">
-										GB
-									</a-select-option>
-									<a-select-option value="MB">
-										MB
-									</a-select-option>
-								</a-select>
-							</a-input-number>
-						</div>
+						<!-- <a-row :gutter="{sm: [0, 10]}"> -->
+						<a-row :gutter="[10, 10]">
+							<a-col :sm="12" :span="24">
+								<a-row>
+									<a-col  :sm="8" :span="12">
+									<!-- <div style="margin-bottom: 16px"> -->
+										RAM: 
+									</a-col>
+									<a-col  :sm="13" :span="12">
+										<a-row>
+											<a-col :span="16">
+												<a-input-number v-model="options.ram.size" class="max-width" default-value="1" />
+											</a-col>
+											<a-col :span="8">
+												<a-select v-model="options.ram.units" class="max-width" default-value="GB">
+													<a-select-option value="GB">
+														GB
+													</a-select-option>
+													<a-select-option value="MB">
+														MB
+													</a-select-option>
+												</a-select>
+											</a-col>
+										</a-row>
+										
+										<!-- <a-select default-value="GB" style="width: 80px">
+											<a-select-option value="GB">
+												GB
+											</a-select-option>
+											<a-select-option value="MB">
+												MB
+											</a-select-option>
+										</a-select> -->
+									</a-col>
+								</a-row>
+							</a-col>
+
+							<a-col :sm="12" :span="24">
+								<a-row>
+									<a-col :sm="8" :span="12">
+										CPU: 
+									</a-col>
+									<a-col :sm="13" :span="12">
+										<a-input-number v-model="options.cpu" class="max-width" default-value="1" />
+									</a-col>
+								</a-row>
+							</a-col>
+
+						</a-row>
 					</div>
 
 					<div class="newCloud__option-field">
 						<a-divider orientation="left">
 							Диски:
 						</a-divider>
+						<a-row :gutter="[10, 10]">
+							<a-col :sm="12" :span="24">
+								<a-row>
+									<a-col  :sm="8" :span="12">
+										Диск: 
+									</a-col>
+									<a-col  :sm="13" :span="12">
+										<a-row>
+											<a-col :span="8">
+												<a-slider v-model="options.disk.size" :min="1" :max="1024" />
+											</a-col>
+											<a-col :span="8">
+												<a-input-number v-model="options.disk.size" class="max-width" default-value="1" />
+											</a-col>
+											<a-col :span="8">
+												<a-select v-model="options.disk.units" class="max-width" default-value="GB">
+													<a-select-option value="GB">
+														GB
+													</a-select-option>
+													<a-select-option value="MB">
+														MB
+													</a-select-option>
+												</a-select>
+											</a-col>
+										</a-row>
+									</a-col>
+								</a-row>
+							</a-col>
+
+							<a-col :sm="12" :span="24">
+								<a-row>
+									<a-col :sm="8" :span="12">
+										Тип диска: 
+									</a-col>
+									<a-col :sm="13" :span="12">
+										<a-select v-model="options.disk.type" class="max-width" default-value="SSD">
+											<a-select-option value="SSD">
+												SSD
+											</a-select-option>
+											<a-select-option value="HDD">
+												HDD
+											</a-select-option>
+										</a-select>
+									</a-col>
+								</a-row>
+							</a-col>
+
+						</a-row>
+					</div>
+
+
+					<div class="newCloud__option-field">
+						<a-divider orientation="left">
+							Сеть:
+						</a-divider>
+						<a-row :gutter="[10, 10]">
+							<a-col :sm="12" :span="24">
+								<a-row>
+									<a-col  :sm="8" :span="12">
+										Публичная сеть: 
+									</a-col>
+									<a-col  :sm="12" :span="12">
+										<a-switch @change="switchChanged('public network')"/>
+									</a-col>
+								</a-row>
+							</a-col>
+
+							<a-col :sm="12" :span="24">
+								<a-row>
+									<a-col :sm="8" :span="12">
+										Локальная сеть: 
+									</a-col>
+									<a-col :sm="12" :span="12">
+										<a-switch @change="switchChanged('local network')"/>
+									</a-col>
+								</a-row>
+							</a-col>
+
+						</a-row>
 					</div>
 				</div>
 			</div>
@@ -72,6 +178,22 @@ export default {
 	data(){
 		return {
 			activeOS: -1,
+			options: {
+				cpu: 1,
+				ram: {
+					size: 1,
+					units: "GB"
+				},
+				disk: {
+					size: 1,
+					units: "GB",
+					type: "SSD"
+				},
+				network: {
+					public: false,
+					local: false
+				}
+			}
 		}
 	},
 	mounted(){
@@ -84,6 +206,19 @@ export default {
 		onSwiper(){
 			console.log("swiper");
 		},
+		switchChanged(name){
+			switch (name) {
+				case 'local network':
+					this.options.network.local = !this.options.network.local;
+					break;
+				case 'public network':
+					this.options.network.public = !this.options.network.public;
+					break;
+			
+				default:
+					break;
+			}
+		}
 	},
 	computed: {
 		templatesArray(){
@@ -116,11 +251,13 @@ export default {
 	.newCloud__inputs{
 		/* background: green; */
 		margin-right: 20px;
-		flex-grow: 72;
+		/* flex-grow: 72; */
+		width: 72%;
 	}
 
 	.newCloud__calculate{
-		flex-grow: 28;
+		/* flex-grow: 28; */
+		width: 28%;
 	}
 
 	.newCloud__field{
@@ -140,15 +277,20 @@ export default {
 	}
 
 	.newCloud__template{
-    display: grid;
-    /* grid-template-columns: repeat(auto-fill, minmax(max-content, 1fr)); */
+    /* display: grid;
     grid-template-columns: repeat(auto-fill, minmax(min-content, 90px));
     grid-auto-rows: min-content;
     grid-gap: 15px;
-		justify-content: space-between;
+		justify-content: space-between; */
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: flex-start;
 	}
 
 	.newCloud__template-item{
+		width: 116px;
+		margin-right: 10px;
+		margin-bottom: 10px;
 		background-color: #fff;
 		/* padding: 10px; */
 		box-shadow:
@@ -192,6 +334,10 @@ export default {
 		color: #fff;
 	}
 
+	.max-width{
+		width: 100%;
+	}
+
 	@media screen and (max-width: 1024px) {
 		.newCloud{
 			flex-direction: column;
@@ -202,6 +348,7 @@ export default {
 		.newCloud__inputs{
 			margin: 0;
 			border-radius: 20px 20px 0 0;
+			width: auto;
 		}
 		.newCloud__field{
 			box-shadow: none;
@@ -209,6 +356,35 @@ export default {
 		}
 		.newCloud__calculate{
 			border-radius: 0 0 20px 20px;
+			width: auto;
+		}
+	}
+
+	@media screen and (max-width: 576px) {
+		.newCloud__template{
+			flex-direction: column;
+			flex-wrap: nowrap;
+			align-items: stretch;
+		}
+		.newCloud__template-item{
+			grid-template-columns: max-content auto;
+			grid-template-rows: 1fr;
+			width: auto;
+			height: 50px;
+		}
+		.newCloud__template-image{
+			width: 50px;
+			height: 50px;
+			padding: 4px;
+		}
+		.newCloud__template-image img{
+			object-fit: contain;
+			width: 100%;
+			height: 100%;
+		}
+		.newCloud__template-name{
+			text-align: left;
+			line-height: 30px;
 		}
 	}
 </style>
