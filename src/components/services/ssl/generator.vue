37ddc2f0b7a16d04ada82a3cef5bddcb29e54852
Alexandr <bsac.po721@gmail.com>
2021-07-23 15:15:09 +0300

    ssl generator start

--- src/components/services/ssl/generator.vue
+++ src/components/services/ssl/generator.vue
@@ -4,14 +4,81 @@
 			<div class="generator__wrapper">
 				<h2>CSR generator</h2>
 
+				<form action="#" class="generator__form">
+					<a-row>
+						<a-col>Domain:</a-col>
+						<a-col>
+							<a-input></a-input>
+						</a-col>
+					</a-row>
+
+					<a-row>
+						<a-col>Organization:</a-col>
+						<a-col>
+							<a-input></a-input>
+						</a-col>
+					</a-row>
+
+					<a-row>
+						<a-col>Departament:</a-col>
+						<a-col>
+							<a-input></a-input>
+						</a-col>
+					</a-row>
+
+					<a-row>
+						<a-col>City:</a-col>
+						<a-col>
+							<a-input></a-input>
+						</a-col>
+					</a-row>
+
+					<a-row>
+						<a-col>State:</a-col>
+						<a-col>
+							<a-input></a-input>
+						</a-col>
+					</a-row>
+
+					<a-row>
+						<a-col>Country:</a-col>
+						<a-col>
+							<a-select style="width: 100%">
+								<a-select-option v-for="country in countries" :key="country" :value="country">
+									{{country}}
+								</a-select-option>
+							</a-select>
+						</a-col>
+					</a-row>
+
+					<a-row>
+						<a-col>Email:</a-col>
+						<a-col>
+							<a-input></a-input>
+						</a-col>
+					</a-row>
+
+					<a-row>
+						<a-col>
+							<a-button type="primary">Generate CSR</a-button>
+						</a-col>
+					</a-row>
+				</form>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
+const countries = ["AD", "AE", "AF", "AG", "AI", "AL", "AM", "AO", "AQ", "AR", "AS", "AT", "AU", "AW", "AX", "AZ", "BA", "BB", "BD", "BE", "BF", "BG", "BH", "BI", "BJ", "BL", "BM", "BN", "BO", "BQ", "BR", "BS", "BT", "BV", "BW", "BY", "BZ", "CA", "CC", "CD", "CF", "CG", "CH", "CI", "CK", "CL", "CM", "CN", "CO", "CR", "CU", "CV", "CW", "CX", "CY", "CZ", "DE", "DJ", "DK", "DM", "DO", "DZ", "EC", "EE", "EG", "EH", "ER", "ES", "ET", "FI", "FJ", "FK", "FM", "FO", "FR", "GA", "GB", "GD", "GE", "GF", "GG", "GH", "GI", "GL", "GM", "GN", "GP", "GQ", "GR", "GS", "GT", "GU", "GW", "GY", "HK", "HM", "HN", "HR", "HT", "HU", "ID", "IE", "IL", "IM", "IN", "IO", "IQ", "IR", "IS", "IT", "JE", "JM", "JO", "JP", "KE", "KG", "KH", "KI", "KM", "KN", "KP", "KR", "KW", "KY", "KZ", "LA", "LB", "LC", "LI", "LK", "LR", "LS", "LT", "LU", "LV", "LY", "MA", "MC", "MD", "ME", "MF", "MG", "MH", "MK", "ML", "MM", "MN", "MO", "MP", "MQ", "MR", "MS", "MT", "MU", "MV", "MW", "MX", "MY", "MZ", "NA", "NC", "NE", "NF", "NG", "NI", "NL", "NO", "NP", "NR", "NU", "NZ", "OM", "PA", "PE", "PF", "PG", "PH", "PK", "PL", "PM", "PN", "PR", "PS", "PT", "PW", "PY", "QA", "RE", "RO", "RS", "RU", "RW", "SA", "SB", "SC", "SD", "SE", "SG", "SH", "SI", "SJ", "SK", "SL", "SM", "SN", "SO", "SR", "SS", "ST", "SV", "SX", "SY", "SZ", "TC", "TD", "TF", "TG", "TH", "TJ", "TK", "TL", "TM", "TN", "TO", "TR", "TT", "TV", "TW", "TZ", "UA", "UG", "UM", "US", "UY", "UZ", "VA", "VC", "VE", "VG", "VI", "VN", "VU", "WF", "WS", "YE", "YT", "ZA", "ZM", "ZW", ];
+
 export default {
 	name: 'ssl-generator',
+	data(){
+		return {
+			countries
+		}
+	}
 }
 </script>
 
