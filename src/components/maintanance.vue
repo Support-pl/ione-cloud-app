c355f14bf21da22f0686eef81cd29c203b4e4b85
Alexandr Korolev <dowellkin@gmail.com>
2022-01-03 13:01:03 +0300

    new maintanance mode image and copyright

--- src/components/maintanance.vue
+++ src/components/maintanance.vue
@@ -1,8 +1,17 @@
 <template>
 	<div class="maintanance">
-		<a-result status="500" title="Maintanance mode" sub-title="Sorry, we will back soon.">
-			<template v-if="mainPageButton" #extra>
-				<router-link :to="{name: 'root'}">
+		<a-result title="Oops...">
+			<template #icon>
+				<a-icon type="tool"></a-icon>
+			</template>
+			<template #subTitle>
+				<p>Something is not working as it should.</p>
+				<p>We already know about this and are taking all necessary measures right now.</p>
+				<p>Thank you for your patience.</p>
+				<p>Soon we will be back online.</p>
+			</template>
+			<template #extra>
+				<router-link v-if="mainPageButton" :to="{name: 'root'}">
 					<a-button type="primary">
 						Back Home
 					</a-button>
@@ -21,11 +30,17 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .maintanance{
 	height: 100%;
 	display: flex;
 	justify-content: center;
 	align-items: center;
+}
+
+p {
+	padding: 0;
+	margin: 0;
+	margin-bottom: 5px;
 }
 </style>
